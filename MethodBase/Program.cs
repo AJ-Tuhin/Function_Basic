45742d351375c829158f56378d89ebfb82f9a7db
Md. Al Jobaed <tuhincse001@gmail.com>
2017-11-25 22:28:46 +0600

    Add files via upload

---
+++ MethodBase/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Function();
+            Console.WriteLine("I am main block.");
+            Function();
+            Console.WriteLine("i am main block again.");
+            Function();
+            Console.ReadKey();
+        }
+
+        static void Function()
+        {
+            Console.WriteLine("I am function !!");
+        }
+    }
+}
