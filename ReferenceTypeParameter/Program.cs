29f2409ed19f0e57a6da987c5b9f1d1204363b1f
Md. Al Jobaed <tuhincse001@gmail.com>
2017-11-25 22:40:07 +0600

    Add files via upload

---
+++ ReferenceTypeParameter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferenceTypeParameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2000;
+            Console.WriteLine(a);
+            Increse(ref a);
+            Console.WriteLine(a);
+            Increse(ref a);
+            Console.WriteLine(a);
+            Console.ReadKey();
+        }
+
+        static void Increse(ref int a)
+        {
+            a += 100;
+            Console.WriteLine(a);
+        }
+    }
+}
