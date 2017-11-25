17a46fadfe00fdcacc5138cacfeacecd86c3eafa
Md. Al Jobaed <tuhincse001@gmail.com>
2017-11-25 22:36:47 +0600

    Add files via upload

---
+++ OutPrameter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutPrameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 200;
+            Console.WriteLine(a);
+            Increse(out a);
+            Console.WriteLine(a);
+
+            Console.ReadKey();
+        }
+
+        static void Increse(out int a)
+        {
+            a = 100;
+            Console.WriteLine(a);
+        }
+
+    }
+}
