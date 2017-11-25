1f6382a03c73c36227cd5615c43b502d1f4cb7ac
Md. Al Jobaed <tuhincse001@gmail.com>
2017-11-25 22:39:22 +0600

    Add files via upload

---
+++ ValueTypeParameter/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueTypeParameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 200;
+            Console.WriteLine(a);
+            Increse(a);
+            Console.WriteLine(a);
+
+            Console.ReadKey();
+        }
+
+        static void Increse(int a)
+        {
+            a += 100;
+            Console.WriteLine(a);
+        }
+    }
+}
